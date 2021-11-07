f846a444e1ec0167ac3285d07a9ad0617c3a6d1f
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-07 19:28:46 -0300

    refactor: changing test files to separated folder & creating additional test helper

---
+++ tests/presentation/helpers/auth-use-case-spy.js
@@ -0,0 +1,7 @@
+module.exports = class AuthUseCaseSpy {
+  async execute (email, password) {
+    this.email = email
+    this.password = password
+    return this.accessToken
+  }
+}
--- tests/presentation/routers/login-router.spec.js
+++ tests/presentation/routers/login-router.spec.js
@@ -1,22 +1,19 @@
-const LoginRouter = require('./login-router')
+const LoginRouter = require('../../../src/presentation/routers/login-router')
 
-const MissingParamError = require('../errors/missing-param-error')
-const UnauthorizedUserError = require('../errors/unauthorized-error')
-const ServerError = require('../errors/server-error')
+const MissingParamError = require('../../../src/presentation/errors/missing-param-error')
+const UnauthorizedUserError = require('../../../src/presentation/errors/unauthorized-error')
+const ServerError = require('../../../src/presentation/errors/server-error')
+
+const AuthUseCaseSpy = require('../helpers/auth-use-case-spy')
 
 const FAKE_GENERIC_PASSWORD = 'any_password'
 const FAKE_GENERIC_EMAIL = 'test@gmail.com'
+const FAKE_ACCESS_TOKEN = 'access_token'
+const INVALID_FAKE_ACCESS_TOKEN = null
 const INVALID_FAKE_GENERIC_PASSWORD = 'invalid_password'
 const INVALID_FAKE_GENERIC_EMAIL = 'invalid_test@gmail.com'
 
 const makeSut = () => {
-  class AuthUseCaseSpy {
-    execute (email, password) {
-      this.email = email
-      this.password = password
-    }
-  }
-
   const authUseCaseSpy = new AuthUseCaseSpy()
   const sut = new LoginRouter(authUseCaseSpy)
 
@@ -27,46 +24,46 @@ const makeSut = () => {
 }
 
 describe('Login Router', () => {
-  it('Should return 400 if no "email" is provided', () => {
+  it('Should return 400 if no "email" is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: FAKE_GENERIC_PASSWORD
       }
     }
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  it('Should return 400 if no "password" is provided', () => {
+  it('Should return 400 if no "password" is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: FAKE_GENERIC_EMAIL
       }
     }
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  it('Should return 500 if no "httpRequest" is provided', () => {
+  it('Should return 500 if no "httpRequest" is provided', async () => {
     const { sut } = makeSut()
-    const httpResponse = sut.route()
+    const httpResponse = await sut.route()
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  it('Should return 500 if no "httpRequest" has no "body"', () => {
+  it('Should return 500 if no "httpRequest" has no "body"', async () => {
     const { sut } = makeSut()
     const httpRequest = {}
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  it('Should call AuthUseCase with correct params', () => {
+  it('Should call AuthUseCase with correct params', async () => {
     const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
@@ -74,25 +71,26 @@ describe('Login Router', () => {
         password: FAKE_GENERIC_PASSWORD
       }
     }
-    sut.route(httpRequest)
+    await sut.route(httpRequest)
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 
-  it('Should return 401 when invalid credentials are provided', () => {
-    const { sut } = makeSut()
+  it('Should return 401 when invalid credentials are provided', async () => {
+    const { sut, authUseCaseSpy } = makeSut()
+    authUseCaseSpy.accessToken = INVALID_FAKE_ACCESS_TOKEN
     const httpRequest = {
       body: {
         email: INVALID_FAKE_GENERIC_EMAIL,
         password: INVALID_FAKE_GENERIC_PASSWORD
       }
     }
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedUserError())
   })
 
-  it('Should return 500 if AuthUseCase is provided', () => {
+  it('Should return 500 if AuthUseCase is provided', async () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -100,12 +98,12 @@ describe('Login Router', () => {
         password: FAKE_GENERIC_PASSWORD
       }
     }
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  it('Should return 500 if AuthUseCase has no execute method', () => {
+  it('Should return 500 if AuthUseCase has no execute method', async () => {
     const sut = new LoginRouter({})
     const httpRequest = {
       body: {
@@ -113,8 +111,21 @@ describe('Login Router', () => {
         password: FAKE_GENERIC_PASSWORD
       }
     }
-    const httpResponse = sut.route(httpRequest)
+    const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  it('Should return 200 when valid credentials are provided', async () => {
+    const { sut, authUseCaseSpy } = makeSut()
+    authUseCaseSpy.accessToken = FAKE_ACCESS_TOKEN
+    const httpRequest = {
+      body: {
+        email: FAKE_GENERIC_EMAIL,
+        password: FAKE_GENERIC_PASSWORD
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
   })
 })
