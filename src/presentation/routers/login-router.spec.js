84d45a5c9ea533827a0d3bbb87e3dbb59b013045
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-07 00:07:28 -0300

    Adding first test cases

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,5 +1,40 @@
-describe('', () => {
-  it('', () => {
+const FAKE_GENERIC_PASSWORD = 'any_password'
+const FAKE_GENERIC_EMAIL = 'test@gmail.com'
 
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    } else if (!httpRequest.body.password) {
+      return {
+        statusCode: 401
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  it('Should return 400 if no "email" is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: FAKE_GENERIC_PASSWORD
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 401 if no "password" is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: FAKE_GENERIC_EMAIL
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
   })
 })
