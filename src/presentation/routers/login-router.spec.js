260e9025b31f8356b4361d33cd4d4f8977a49f20
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-07 14:25:50 -0300

    test: adding test to ensure that LoginRouter will return 401 status

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,11 +1,13 @@
 const LoginRouter = require('./login-router')
 
 const MissingParamError = require('../errors/missing-param-error')
-// const UnauthorizedUserError = require('../errors/unauthorized-error')
+const UnauthorizedUserError = require('../errors/unauthorized-error')
 const ServerError = require('../errors/server-error')
 
 const FAKE_GENERIC_PASSWORD = 'any_password'
 const FAKE_GENERIC_EMAIL = 'test@gmail.com'
+const INVALID_FAKE_GENERIC_PASSWORD = 'invalid_password'
+const INVALID_FAKE_GENERIC_EMAIL = 'invalid_test@gmail.com'
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -75,5 +77,18 @@ describe('Login Router', () => {
     sut.route(httpRequest)
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
+  })
+
+  it('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: INVALID_FAKE_GENERIC_EMAIL,
+        password: INVALID_FAKE_GENERIC_PASSWORD
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new UnauthorizedUserError())
   })
 })
