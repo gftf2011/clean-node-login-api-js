6310707dc1bf971ddf49200c2b8635192b07c4a2
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-08 13:46:36 -0300

    test: ensure EmailValidator returns false if 'validator' returns false

--- tests/utils/email-validator.spec.js
+++ tests/utils/email-validator.spec.js
@@ -1,14 +1,31 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    this.email = email
-    return true
+    return validator.isEmail(email)
   }
 }
 
+const makeSut = () => {
+  const sut = new EmailValidator()
+  return { sut }
+}
+
+const VALID_EMAIL = 'validtest001@gmail.com'
+const INVALID_EMAIL = 'invalidtest001@gmail.com'
+
 describe('Email Validator', () => {
   it('Should return "true" if validator returns "true"', () => {
-    const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('test001@gmail.com')
+    validator.isEmailValid = true
+    const { sut } = makeSut()
+    const isEmailValid = sut.isValid(VALID_EMAIL)
     expect(isEmailValid).toBe(true)
+  })
+
+  it('Should return "false" if validator returns "false"', () => {
+    validator.isEmailValid = false
+    const { sut } = makeSut()
+    const isEmailValid = sut.isValid(INVALID_EMAIL)
+    expect(isEmailValid).toBe(false)
   })
 })
