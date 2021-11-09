71b0807cb2e96062f54b49451ffd2f69c8502ab2
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-08 22:07:38 -0300

    test: ensure that in AuthUseCase if no password is provided than an error thrown

--- tests/domain/use-cases/auth-use-case.spec.js
+++ tests/domain/use-cases/auth-use-case.spec.js
@@ -4,10 +4,13 @@ class AuthUseCase {
   async execute (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    } else if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
 
+const FAKE_GENERIC_EMAIL = 'test@gmail.com'
 const FAKE_GENERIC_PASSWORD = 'any_password'
 
 const makeSut = () => {
@@ -20,5 +23,11 @@ describe('Auth UseCase', () => {
     const { sut } = makeSut()
     const promise = sut.execute(undefined, FAKE_GENERIC_PASSWORD)
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  it('Should throw error if no password is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.execute(FAKE_GENERIC_EMAIL, undefined)
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
