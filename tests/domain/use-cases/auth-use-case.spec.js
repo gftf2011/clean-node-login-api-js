faa116108ea2d87b6d0667c6b268d719bc4ceb6d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-08 23:27:56 -0300

    test: ensure that AuthUseCase calls LoadUserByEmailRepository with correct email

--- tests/domain/use-cases/auth-use-case.spec.js
+++ tests/domain/use-cases/auth-use-case.spec.js
@@ -1,12 +1,23 @@
 const MissingParamError = require('../../../src/utils/errors/missing-param-error')
 
+class LoadUserByEmailRepositorySpy {
+  async load (email) {
+    this.email = email
+  }
+}
+
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async execute (email, password) {
     if (!email) {
       throw new MissingParamError('email')
     } else if (!password) {
       throw new MissingParamError('password')
     }
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
@@ -14,20 +25,30 @@ const FAKE_GENERIC_EMAIL = 'test@gmail.com'
 const FAKE_GENERIC_PASSWORD = 'any_password'
 
 const makeSut = () => {
-  const sut = new AuthUseCase()
-  return { sut }
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  return {
+    sut,
+    loadUserByEmailRepositorySpy
+  }
 }
 
 describe('Auth UseCase', () => {
-  it('Should throw error if no email is provided', async () => {
+  it('Should throw error if no email is provided', () => {
     const { sut } = makeSut()
     const promise = sut.execute(undefined, FAKE_GENERIC_PASSWORD)
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
-  it('Should throw error if no password is provided', async () => {
+  it('Should throw error if no password is provided', () => {
     const { sut } = makeSut()
     const promise = sut.execute(FAKE_GENERIC_EMAIL, undefined)
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  it('Should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    await sut.execute(FAKE_GENERIC_EMAIL, FAKE_GENERIC_PASSWORD)
+    expect(FAKE_GENERIC_EMAIL).toBe(loadUserByEmailRepositorySpy.email)
   })
 })
