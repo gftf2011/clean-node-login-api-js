f13a2f99593c1148eaa979896b7df45e7c4068d7
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-10 23:41:12 -0300

    refactor: simplify code with factory helpers

--- src/domain/use-cases/auth-use-case.js
+++ src/domain/use-cases/auth-use-case.js
@@ -19,7 +19,9 @@ module.exports = class AuthUseCase {
     if (!user) {
       return null
     }
-    await this.encrypter.compare(password, user.password)
-    return null
+    const isValid = await this.encrypter.compare(password, user.password)
+    if (!isValid) {
+      return null
+    }
   }
 }
--- tests/domain/use-cases/auth-use-case.spec.js
+++ tests/domain/use-cases/auth-use-case.spec.js
@@ -3,45 +3,42 @@ const ServerError = require('../../../src/utils/errors/server-error')
 
 const AuthUseCase = require('../../../src/domain/use-cases/auth-use-case')
 
-class LoadUserByEmailRepositorySpy {
-  async load (email) {
-    this.email = email
-    return this.user
-  }
-}
-
-class EncrypterSpy {
-  async compare (password, hashedPassword) {
-    this.password = password
-    this.hashedPassword = hashedPassword
-  }
-}
-
 const FAKE_GENERIC_EMAIL = 'test@gmail.com'
 const FAKE_GENERIC_PASSWORD = 'any_password'
 const FAKE_HASHED_PASSWORD = 'hashed_password'
 const INVALID_FAKE_GENERIC_EMAIL = 'invalid_test@gmail.com'
 const INVALID_FAKE_GENERIC_PASSWORD = 'invalid_password'
 
-const makeSut = () => {
+const createEncrypterSpyFactory = () => {
+  class EncrypterSpy {
+    async compare (password, hashedPassword) {
+      this.password = password
+      this.hashedPassword = hashedPassword
+      return this.isValid
+    }
+  }
   const encrypterSpy = new EncrypterSpy()
+  encrypterSpy.isValid = true
+  return encrypterSpy
+}
+
+const createLoadUserByEmailRepositorySpyFactory = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load (email) {
+      this.email = email
+      return this.user
+    }
+  }
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  // User body format still unknown so an empty object will be used temporarily
   loadUserByEmailRepositorySpy.user = {
     password: FAKE_HASHED_PASSWORD
   }
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
-  return {
-    sut,
-    loadUserByEmailRepositorySpy,
-    encrypterSpy
-  }
+  return loadUserByEmailRepositorySpy
 }
 
-const makeSutWithNoUser = () => {
-  const encrypterSpy = new EncrypterSpy()
-  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
-  loadUserByEmailRepositorySpy.user = null
+const createSutFactory = () => {
+  const encrypterSpy = createEncrypterSpyFactory()
+  const loadUserByEmailRepositorySpy = createLoadUserByEmailRepositorySpyFactory()
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy)
   return {
     sut,
@@ -52,19 +49,19 @@ const makeSutWithNoUser = () => {
 
 describe('Auth UseCase', () => {
   it('Should throw error if no email is provided', () => {
-    const { sut } = makeSut()
+    const { sut } = createSutFactory()
     const promise = sut.execute(undefined, FAKE_GENERIC_PASSWORD)
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   it('Should throw error if no password is provided', () => {
-    const { sut } = makeSut()
+    const { sut } = createSutFactory()
     const promise = sut.execute(FAKE_GENERIC_EMAIL, undefined)
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   it('Should call LoadUserByEmailRepository with correct email', async () => {
-    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    const { sut, loadUserByEmailRepositorySpy } = createSutFactory()
     await sut.execute(FAKE_GENERIC_EMAIL, FAKE_GENERIC_PASSWORD)
     expect(FAKE_GENERIC_EMAIL).toBe(loadUserByEmailRepositorySpy.email)
   })
@@ -82,19 +79,21 @@ describe('Auth UseCase', () => {
   })
 
   it('Should return null if invalid email is provided', async () => {
-    const { sut } = makeSutWithNoUser()
+    const { sut, loadUserByEmailRepositorySpy } = createSutFactory()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.execute(INVALID_FAKE_GENERIC_EMAIL, FAKE_GENERIC_PASSWORD)
     expect(accessToken).toBeNull()
   })
 
   it('Should return null if invalid password is provided', async () => {
-    const { sut } = makeSut()
+    const { sut, encrypterSpy } = createSutFactory()
+    encrypterSpy.isValid = false
     const accessToken = await sut.execute(FAKE_GENERIC_EMAIL, INVALID_FAKE_GENERIC_PASSWORD)
     expect(accessToken).toBeNull()
   })
 
   it('Should call Encrypter with correct values', async () => {
-    const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = makeSut()
+    const { sut, loadUserByEmailRepositorySpy, encrypterSpy } = createSutFactory()
     await sut.execute(FAKE_GENERIC_EMAIL, FAKE_GENERIC_PASSWORD)
     expect(encrypterSpy.password).toBe(FAKE_GENERIC_PASSWORD)
     expect(encrypterSpy.hashedPassword).toBe(loadUserByEmailRepositorySpy.user.password)
