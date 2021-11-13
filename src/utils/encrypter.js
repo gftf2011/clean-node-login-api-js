ad599389b5ba76d0be0fce236f06e60c6cabd539
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-13 17:58:44 -0300

    test: ensure Encrypter throws MissingParamError if no value is provided

--- src/utils/encrypter.js
+++ src/utils/encrypter.js
@@ -1,8 +1,13 @@
 const bcrypt = require('bcrypt')
 
+const MissingParamError = require('./errors/missing-param-error')
+
 module.exports = class Encrypter {
-  async compare (password, hashedPassword) {
-    const isValid = await bcrypt.compare(password, hashedPassword)
+  async compare (value, hashValue) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+    const isValid = await bcrypt.compare(value, hashValue)
     return isValid
   }
 }
--- tests/utils/encrypter.spec.js
+++ tests/utils/encrypter.spec.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt')
 
+const MissingParamError = require('../../src/utils/errors/missing-param-error')
+
 const Encrypter = require('../../src/utils/encrypter')
 
 const FAKE_GENERIC_PASSWORD = 'any_password'
@@ -8,17 +10,15 @@ const FAKE_WRONG_HASHED_PASSWORD = 'wrong_hashed_password'
 
 const createSutFactory = () => {
   const sut = new Encrypter()
-  return {
-    sut
-  }
+  return { sut }
 }
 
 describe('Encrypter', () => {
   it('Should call bcrypt with correct values', async () => {
     const { sut } = createSutFactory()
     await sut.compare(FAKE_GENERIC_PASSWORD, FAKE_HASHED_PASSWORD)
-    expect(bcrypt.password).toBe(FAKE_GENERIC_PASSWORD)
-    expect(bcrypt.hashedPassword).toBe(FAKE_HASHED_PASSWORD)
+    expect(bcrypt.value).toBe(FAKE_GENERIC_PASSWORD)
+    expect(bcrypt.hashValue).toBe(FAKE_HASHED_PASSWORD)
   })
 
   it('Should return "true" if bcrypt returns "true"', async () => {
@@ -32,5 +32,11 @@ describe('Encrypter', () => {
     const { sut } = createSutFactory()
     const isValid = await sut.compare(FAKE_GENERIC_PASSWORD, FAKE_WRONG_HASHED_PASSWORD)
     expect(isValid).toBe(false)
+  })
+
+  it('Should throw MissingParamError if no value is not provided', () => {
+    const { sut } = createSutFactory()
+    const promise = sut.compare(undefined, FAKE_HASHED_PASSWORD)
+    expect(promise).rejects.toThrow(new MissingParamError('value'))
   })
 })
