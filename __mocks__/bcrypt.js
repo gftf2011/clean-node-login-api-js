2ff164d53b5fc648b448a3cb9e74cc527721d5df
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-13 17:58:44 -0300

    test: ensure Encrypter throws MissingParamError if no value is provided

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,8 +1,8 @@
 module.exports = {
   isValid: true,
-  async compare (password, hashedPassword) {
-    this.password = password
-    this.hashedPassword = hashedPassword
+  async compare (value, hashValue) {
+    this.value = value
+    this.hashValue = hashValue
     return this.isValid
   }
 }
