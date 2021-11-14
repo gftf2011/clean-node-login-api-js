8cb077057907a4de90552531e011f2f3095a0bfa
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-14 17:07:24 -0300

    refactor: better SUT definition with abstract factory usage

--- tests/utils/validators/email-validator.spec.js
+++ tests/utils/validators/email-validator.spec.js
@@ -4,9 +4,11 @@ const MissingParamError = require('../../../src/utils/errors/missing-param-error
 
 const EmailValidator = require('../../../src/utils/validators/email-validator')
 
-const createSutFactory = () => {
-  const sut = new EmailValidator()
-  return { sut }
+class SutFactory {
+  create () {
+    this.sut = new EmailValidator()
+    return { sut: this.sut }
+  }
 }
 
 const VALID_EMAIL = 'validtest001@gmail.com'
@@ -14,27 +16,27 @@ const INVALID_EMAIL = 'invalidtest001@gmail.com'
 
 describe('Email Validator', () => {
   it('Should call validator with correct email', () => {
-    const { sut } = createSutFactory()
+    const { sut } = new SutFactory().create()
     sut.isValid(VALID_EMAIL)
     expect(validator.email).toBe(VALID_EMAIL)
   })
 
   it('Should return "true" if validator returns "true"', () => {
     validator.isEmailValid = true
-    const { sut } = createSutFactory()
+    const { sut } = new SutFactory().create()
     const isEmailValid = sut.isValid(VALID_EMAIL)
     expect(isEmailValid).toBe(true)
   })
 
   it('Should return "false" if validator returns "false"', () => {
     validator.isEmailValid = false
-    const { sut } = createSutFactory()
+    const { sut } = new SutFactory().create()
     const isEmailValid = sut.isValid(INVALID_EMAIL)
     expect(isEmailValid).toBe(false)
   })
 
   it('Should throws MissingParamError if no email is provided', () => {
-    const { sut } = createSutFactory()
+    const { sut } = new SutFactory().create()
     expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
