305eca6e79de5817df99c5d02667ff8bb6d53e48
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-15 01:20:50 -0300

    test: ensure LoadUserByEmailRepository returns user if user is found

--- tests/infra/repositories/load-user-by-email-repository.spec.js
+++ tests/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,20 +1,52 @@
+const { MongoClient } = require('mongodb')
+
+const FAKE_GENERIC_EMAIL = 'test@gmail.com'
 const INVALID_FAKE_GENERIC_EMAIL = 'invalid_test@gmail.com'
 
 class LoadUserByEmailRepository {
+  constructor ({ userModel }) {
+    this.userModel = userModel
+  }
+
   async load (email) {
-    return null
+    const user = await this.userModel.findOne({ email })
+    return user
   }
 }
 
-const createSutFactory = () => {
-  const sut = new LoadUserByEmailRepository()
-  return { sut }
-}
-
 describe('LoadUserByEmail Repository', () => {
+  let connection, db
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = connection.db()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
   it('Should return null if no user is found', async () => {
-    const { sut } = createSutFactory()
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository({ userModel })
     const user = await sut.load(INVALID_FAKE_GENERIC_EMAIL)
     expect(user).toBeNull()
+  })
+
+  it('Should return user if an user is found', async () => {
+    const userModel = db.collection('users')
+    await userModel.insertOne({
+      email: FAKE_GENERIC_EMAIL
+    })
+    const sut = new LoadUserByEmailRepository({ userModel })
+    const user = await sut.load(FAKE_GENERIC_EMAIL)
+    expect(user.email).toBe(FAKE_GENERIC_EMAIL)
+  })
+
+  afterAll(async () => {
+    await connection.close()
   })
 })
