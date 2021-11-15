2a17c60af5a50858f50550999062f609522d150e
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-15 18:56:03 -0300

    test: ensure LoadUserByEmailRepository throws MissingParamError if no email is provided

--- src/infra/repositories/load-user-by-email-repository.js
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -1,4 +1,5 @@
 const ServerError = require('../../../src/utils/errors/server-error')
+const MissingParamError = require('../../utils/errors/missing-param-error')
 
 module.exports = class LoadUserByEmailRepository {
   constructor ({ userModel } = {}) {
@@ -8,6 +9,8 @@ module.exports = class LoadUserByEmailRepository {
   async load (email) {
     if (!this.userModel || !this.userModel.findOne) {
       throw new ServerError()
+    } else if (!email) {
+      throw new MissingParamError('email')
     }
     const user = await this.userModel.findOne({ email })
     return user
