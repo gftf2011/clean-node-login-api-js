b2eb6a4bc3017e404b5262a29f4f511e2262fde0
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-15 19:11:08 -0300

    refactor: moving SUT factory methods to separated folders

---
+++ tests/utils/validators/abstract-factories/sut-factory.js
@@ -0,0 +1,8 @@
+const EmailValidator = require('../../../../src/utils/validators/email-validator')
+
+module.exports = class SutFactory {
+  create () {
+    this.sut = new EmailValidator()
+    return { sut: this.sut }
+  }
+}
