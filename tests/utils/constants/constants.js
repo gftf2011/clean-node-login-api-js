e60ca741b65b381d5b576b86e870218395319238
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-15 19:16:38 -0300

    refactor: moving SUT abstract-factories to separated folders

---
+++ tests/utils/constants/constants.js
@@ -0,0 +1,9 @@
+const FAKE_GENERIC_PASSWORD = 'any_password'
+const FAKE_HASHED_PASSWORD = 'hashed_password'
+const FAKE_WRONG_HASHED_PASSWORD = 'wrong_hashed_password'
+
+module.exports = {
+  FAKE_GENERIC_PASSWORD,
+  FAKE_HASHED_PASSWORD,
+  FAKE_WRONG_HASHED_PASSWORD
+}
