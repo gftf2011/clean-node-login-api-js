a8bdd89313cee1cc9be7e5ce14db14539e89b14f
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-16 03:43:59 -0300

    test: ensure MongoHelper has default retryDisconnect value when args dependency is not provided

--- tests/infra/helpers/mongo-helper.spec.js
+++ tests/infra/helpers/mongo-helper.spec.js
@@ -6,13 +6,13 @@ describe('Mongo Helper', () => {
     process.env.MONGO_DISCONNECT_RETRY = '2'
   })
 
-  it('it Should set retryConnect property with default env value when args dependencie is not provided', () => {
+  it('it Should set retryConnect property with default env value when args dependency is not provided', () => {
     const sut = new MongoHelper()
     expect(sut.retryConnect).toBe(parseInt(process.env.MONGO_CONNECT_RETRY = '2', 10))
   })
 
-  // it('it Should set retryDisconnect property with default env value when args dependencie is not provided', () => {
-  //   const sut = new MongoHelper()
-  //   expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
-  // })
+  it('it Should set retryDisconnect property with default env value when args dependencie is not provided', () => {
+    const sut = new MongoHelper()
+    expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
+  })
 })
