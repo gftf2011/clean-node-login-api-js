5a95d5d2f2bd36dad58b545d461856c65d1eb6b9
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-16 04:01:08 -0300

    test: ensure MongoHelper sets retryConnect with correct value when args dependency is provided

--- tests/infra/helpers/mongo-helper.spec.js
+++ tests/infra/helpers/mongo-helper.spec.js
@@ -1,5 +1,7 @@
 const MongoHelper = require('../../../src/infra/helpers/mongo-helper')
 
+const MONGO_ATTEMPTS_TO_RETRY = 3
+
 describe('Mongo Helper', () => {
   beforeAll(() => {
     process.env.MONGO_CONNECT_RETRY = '2'
@@ -11,18 +13,23 @@ describe('Mongo Helper', () => {
     expect(sut.retryConnect).toBe(parseInt(process.env.MONGO_CONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryDisconnect property with default env value when args dependencie is not provided', () => {
+  it('it Should set retryDisconnect property with default env value when args dependency is not provided', () => {
     const sut = new MongoHelper()
     expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryConnect property with default env value when args dependencie is an empty object', () => {
+  it('it Should set retryConnect property with default env value when args dependency is an empty object', () => {
     const sut = new MongoHelper({})
     expect(sut.retryConnect).toBe(parseInt(process.env.MONGO_CONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryDisconnect property with default env value when args dependencie is an empty object', () => {
+  it('it Should set retryDisconnect property with default env value when args dependency is an empty object', () => {
     const sut = new MongoHelper({})
     expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
+  })
+
+  it('it Should set retryConnect property with correct value when args dependency is provided', () => {
+    const sut = new MongoHelper({ attempts: MONGO_ATTEMPTS_TO_RETRY })
+    expect(sut.retryConnect).toBe(MONGO_ATTEMPTS_TO_RETRY)
   })
 })
