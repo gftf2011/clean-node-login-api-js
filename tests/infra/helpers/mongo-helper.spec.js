81483b74b6179dca034c3973b5be703cfb68cf41
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-16 05:16:25 -0300

    test: ensure MongoHelper has client and db if Driver constructed with success

--- tests/infra/helpers/mongo-helper.spec.js
+++ tests/infra/helpers/mongo-helper.spec.js
@@ -1,6 +1,23 @@
 const MongoHelper = require('../../../src/infra/helpers/mongo-helper')
 
 const MONGO_ATTEMPTS_TO_RETRY = 3
+const MONGO_FAKE_URI = 'any_uri'
+const MONGO_FAKE_DATABASE_NAME = 'any_database'
+
+jest.mock('../../../src/infra/helpers/directors/driver.js', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      construct: async (_builder) => {
+        return {
+          client: {
+            close: async () => {}
+          },
+          db: {}
+        }
+      }
+    }
+  })
+})
 
 describe('Mongo Helper', () => {
   beforeAll(() => {
@@ -8,33 +25,41 @@ describe('Mongo Helper', () => {
     process.env.MONGO_DISCONNECT_RETRY = '2'
   })
 
-  it('it Should set retryConnect property with default env value when args dependency is not provided', () => {
+  it('Should set retryConnect property with default env value when args dependency is not provided', () => {
     const sut = new MongoHelper()
     expect(sut.retryConnect).toBe(parseInt(process.env.MONGO_CONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryDisconnect property with default env value when args dependency is not provided', () => {
+  it('Should set retryDisconnect property with default env value when args dependency is not provided', () => {
     const sut = new MongoHelper()
     expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryConnect property with default env value when args dependency is an empty object', () => {
+  it('Should set retryConnect property with default env value when args dependency is an empty object', () => {
     const sut = new MongoHelper({})
     expect(sut.retryConnect).toBe(parseInt(process.env.MONGO_CONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryDisconnect property with default env value when args dependency is an empty object', () => {
+  it('Should set retryDisconnect property with default env value when args dependency is an empty object', () => {
     const sut = new MongoHelper({})
     expect(sut.retryDisconnect).toBe(parseInt(process.env.MONGO_DISCONNECT_RETRY = '2', 10))
   })
 
-  it('it Should set retryConnect property with correct value when args dependency is provided', () => {
+  it('Should set retryConnect property with correct value when args dependency is provided', () => {
     const sut = new MongoHelper({ attempts: MONGO_ATTEMPTS_TO_RETRY })
     expect(sut.retryConnect).toBe(MONGO_ATTEMPTS_TO_RETRY)
   })
 
-  it('it Should set retryDisconnect property with correct value when args dependency is provided', () => {
+  it('Should set retryDisconnect property with correct value when args dependency is provided', () => {
     const sut = new MongoHelper({ attempts: MONGO_ATTEMPTS_TO_RETRY })
     expect(sut.retryDisconnect).toBe(MONGO_ATTEMPTS_TO_RETRY)
+  })
+
+  it('Should build client and db if Driver constructed with success', async () => {
+    const sut = new MongoHelper({ attempts: MONGO_ATTEMPTS_TO_RETRY })
+    await sut.connect(MONGO_FAKE_URI, MONGO_FAKE_DATABASE_NAME)
+
+    expect(sut.client).toHaveProperty('close')
+    expect(sut.db).toEqual({})
   })
 })
--- tests/infra/repositories/load-user-by-email-repository.spec.js
+++ tests/infra/repositories/load-user-by-email-repository.spec.js
@@ -10,7 +10,7 @@ const {
   INVALID_FAKE_GENERIC_EMAIL,
   LOAD_USER_BY_EMAIL_REPOSITORY_EMPTY_SUT,
   LOAD_USER_BY_EMAIL_REPOSITORY_EMPTY_OBJECT_SUT
-} = require('../helpers/constants/constants')
+} = require('../helpers/constants/load-user-by-email-repository-constants')
 
 let db
 
