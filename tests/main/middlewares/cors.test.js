8aab1ee34bd9aa6ceb325e32a392fa0b41b37b75
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-19 01:35:20 -0300

    test: ensure JSON Parser Middleware parse the JSON request

--- tests/main/middlewares/cors.test.js
+++ tests/main/middlewares/cors.test.js
@@ -2,12 +2,12 @@ const request = require('supertest')
 
 const app = require('../../../src/main/server/app')
 
-describe('Cors Middleware', () => {
+describe('CORS Middleware', () => {
   it('Should enable CORS', async () => {
-    app.get('/api/enable-cors', (_req, res) => {
+    app.get('/api/test-enable-cors', (_req, res) => {
       res.send('')
     })
-    const response = await request(app).get('/api/enable-cors')
+    const response = await request(app).get('/api/test-enable-cors')
 
     expect(response.headers['access-control-allow-origin']).toBe('*')
     expect(response.headers['access-control-allow-methods']).toBe('*')
---
+++ tests/main/middlewares/json-parser.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+
+const app = require('../../../src/main/server/app')
+
+describe('JSON Parser Middleware', () => {
+  it('Should parse JSON request', async () => {
+    app.post('/api/test-json-parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app).post('/api/test-json-parser').send({ test: true }).expect({ test: true })
+  })
+})
