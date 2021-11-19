3bb58fdce300bf2a3a768575f57076ea1bcb4035
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-18 22:50:29 -0300

    test: ensure App Setup has no x-powered-by header response

---
+++ tests/main/config/setup.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+
+const app = require('../../../src/main/config/app')
+
+describe('App Setup', () => {
+  it('Should disable "X-Powered-By" response header', async () => {
+    app.get('/api/test-x-powered-by', (_req, res) => {
+      res.send('')
+    })
+    const response = await request(app).get('/api/test-x-powered-by')
+
+    expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+})
