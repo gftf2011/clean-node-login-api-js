0e7fc5e85b7f5d891b4b1295bdf9868acc6f5c96
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-20 20:26:40 -0300

    test: ensure Content-Type has JSON as default return value

---
+++ src/main/middlewares/content-type.js
@@ -0,0 +1,6 @@
+module.exports = app => {
+  app.use((_req, res, next) => {
+    res.type('json')
+    next()
+  })
+}
---
+++ tests/main/middlewares/content-type.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+
+const app = require('../../../src/main/server/app')
+
+describe('Content-Type Middleware', () => {
+  it('Should return json content-type as default', async () => {
+    app.get('/api/test-content-type', (_req, res) => {
+      res.send('')
+    })
+    await request(app).get('/api/test-content-type').expect('content-type', /json/)
+  })
+})
