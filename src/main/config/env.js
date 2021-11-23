e0dc35ae7c8d97bb3627a547e666ee8d3f69b402
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-11-23 19:54:39 -0300

    feat: creating composer for login router & express router adapter

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,7 +1,8 @@
 module.exports = {
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/',
   MONGO_DATABASE: process.env.MONGO_DATABASE || 'clean_node_login_api',
   MONGO_RETRY: process.env.MONGO_CONNECT_RETRY
     ? parseInt(process.env.MONGO_CONNECT_RETRY, 10)
     : 2,
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'secret',
 };
--- src/main/server/app.js
+++ src/main/server/app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 const setup = require('../config/setup');
-const routes = require('../config/routes');
 
 setup(app);
-routes(app);
 
 module.exports = app;
