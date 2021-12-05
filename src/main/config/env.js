ff5c35f509aa912874827d6d931bd4e9d458fa7b
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-12-05 01:35:01 -0300

    refactor: adding APPLICATION_PORT to config env file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -6,4 +6,7 @@ module.exports = {
     ? parseInt(process.env.MONGO_CONNECT_RETRY, 10)
     : 2,
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'secret',
+  APPLICATION_PORT: process.env.APPLICATION_PORT
+    ? parseInt(process.env.APPLICATION_PORT, 10)
+    : 3333,
 };
