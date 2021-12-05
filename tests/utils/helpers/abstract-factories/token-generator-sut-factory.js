59ce6672e509d4fb631fe7c8389c4739c0137284
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-12-05 04:18:35 -0300

    refactor: adding dynamic test values to encrypter.spec.js file

--- tests/utils/helpers/abstract-factories/token-generator-sut-factory.js
+++ tests/utils/helpers/abstract-factories/token-generator-sut-factory.js
@@ -1,10 +1,9 @@
+const faker = require('faker');
 const TokenGenerator = require('../../../../src/utils/generators/token-generator');
-
-const { FAKE_GENERIC_SECRET } = require('../constants');
 
 module.exports = class SutFactory {
   create() {
-    this.sut = new TokenGenerator({ secret: FAKE_GENERIC_SECRET });
+    this.sut = new TokenGenerator({ secret: faker.datatype.uuid() });
     return { sut: this.sut };
   }
 };
