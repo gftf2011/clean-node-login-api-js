ebff8e3ee3598b316183b43c2d48637d436c4b8b
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-12-10 05:46:49 -0300

    test: ensure Morgan Logger middleware logs request

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,11 @@
 const cors = require('../middlewares/cors');
 const jsonParser = require('../middlewares/json-parser');
 const contentType = require('../middlewares/content-type');
+const morgan = require('../middlewares/morgan');
 
 module.exports = app => {
   app.disable('x-powered-by');
+  morgan(app);
   cors(app);
   jsonParser(app);
   contentType(app);
