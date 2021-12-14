9264718187a5016c9215880e122a291c08d1a662
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-12-13 23:53:22 -0300

    test: ensure LogOutRouter returns 400 when TokenValidator throws MissingParamError

--- tests/presentation/helpers/factory-methods/logout-router-sut-factory.js
+++ tests/presentation/helpers/factory-methods/logout-router-sut-factory.js
@@ -1,10 +1,23 @@
 const LogOutRouter = require('../../../../src/presentation/routers/logout-router');
+const MissingParamError = require('../../../../src/utils/errors/missing-param-error');
 
 const DependenciesFactory = require('../abstract-factories/logout-router-dependencies-factory');
 
+const {
+  LOGOUT_ROUTER_SUT_TOKEN_VALIDATOR_NO_TOKEN_ERROR,
+} = require('../constants');
+
 module.exports = class SutFactory {
-  create(_type) {
+  create(type) {
     this.dependencies = new DependenciesFactory().create();
+
+    if (type === LOGOUT_ROUTER_SUT_TOKEN_VALIDATOR_NO_TOKEN_ERROR) {
+      this.dependencies.tokenValidatorSpy.retrieveUserId = async token => {
+        this.dependencies.tokenValidatorSpy.token = token;
+        throw new MissingParamError('token');
+      };
+    }
+
     this.sut = new LogOutRouter({
       tokenValidator: this.dependencies.tokenValidatorSpy,
       logOutUseCase: this.dependencies.logOutUseCaseSpy,
