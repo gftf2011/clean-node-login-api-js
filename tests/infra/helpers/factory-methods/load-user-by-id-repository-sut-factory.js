f29fb2c2cde367a59a90d1b7be1ba28a9d8ec665
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2021-12-27 22:44:34 -0300

    test: ensure LoadUserByIdRepository throws error if userModel has been provided as empty object

--- tests/infra/helpers/factory-methods/load-user-by-id-repository-sut-factory.js
+++ tests/infra/helpers/factory-methods/load-user-by-id-repository-sut-factory.js
@@ -1,6 +1,9 @@
 const LoadUserByIdRepository = require('../../../../src/infra/repositories/load-user-by-id-repository');
 
-const { LOAD_USER_BY_ID_REPOSITORY_SUT_EMPTY } = require('../constants');
+const {
+  LOAD_USER_BY_ID_REPOSITORY_SUT_EMPTY,
+  LOAD_USER_BY_ID_REPOSITORY_SUT_EMPTY_OBJECT,
+} = require('../constants');
 
 module.exports = class SutFactory {
   constructor(db) {
@@ -12,6 +15,8 @@ module.exports = class SutFactory {
 
     if (type === LOAD_USER_BY_ID_REPOSITORY_SUT_EMPTY) {
       this.sut = new LoadUserByIdRepository();
+    } else if (type === LOAD_USER_BY_ID_REPOSITORY_SUT_EMPTY_OBJECT) {
+      this.sut = new LoadUserByIdRepository({});
     } else {
       this.sut = new LoadUserByIdRepository({ userModel: this.userModel });
     }
